cbf06b333a916ab02e93586e652c2b2c1c66f25d
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2024-10-14 05:57:40 +0300

    update login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,6 +77,7 @@ class AuthController extends Controller
             'message' => 'Login successful',
             'user' => auth()->user(),
             'role' => auth()->user()->roles[0]->name,
+            'permissions' => auth()->user()->permissions[0]->name,
             'shift' => $shift,
             'access_token' => $token,
             'token_type' => 'bearer',
